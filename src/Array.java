526946cd67fe830817b63804579ced45bc022d61
Shruti <shrutiswift@gmail.com>
2022-09-01 11:26:41 +0530

    Arraylist

---
+++ src/Array.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        ArrayList <Integer> a = new ArrayList<>();
+        a.add(n.nextInt());
+        System.out.println(a);
+    }
+
+}
