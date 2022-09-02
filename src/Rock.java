ced9b724fae53555fc16ad6e8e2926e5839b0321
Shruti <shrutiswift@gmail.com>
2022-09-02 23:17:19 +0530

    Palindrome

---
+++ src/Rock.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Rock {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String n = s.nextLine();
+        int size, i;
+        for( i = 0; i < n.length(); i++) {
+            if(str.charAt[0] == str.charAt[n.length()-1]){
+
+            }
+            else {
+
+            }
+        }
+//i[0] == i[n-1]
+//the number is not palindrome
+// then i[1] == i [n-2]
+    }
+}
