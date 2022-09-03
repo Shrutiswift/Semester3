d281b16092919e4ab9f41968728573752c106b74
Shruti <shrutiswift@gmail.com>
2022-09-03 22:13:25 +0530

    Palindrome

--- src/Rock.java
+++ src/Rock.java
@@ -6,12 +6,7 @@ public class Rock {
         String n = s.nextLine();
         int size, i;
         for( i = 0; i < n.length(); i++) {
-            if(str.charAt[0] == str.charAt[n.length()-1]){
 
-            }
-            else {
-
-            }
         }
 //i[0] == i[n-1]
 //the number is not palindrome
